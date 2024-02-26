38fdcaba8cddc6aa9d8473a3b625ff0627d8287c
Edvando Simplicio <edvandosimplicio0508@gmail.com>
2024-02-26 16:31:47 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-blog-in-angular-14.1.2';
+}
