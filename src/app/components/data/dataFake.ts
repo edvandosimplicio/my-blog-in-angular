a3bcf2274e4fcadafc8bff88febabb641daf85b5
Edvando Simplicio <edvandosimplicio0508@gmail.com>
2024-02-26 22:47:09 -0300

    content created

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"TITULO DO CARD",
+    "description": "ola",
+    "photoCover":"https://multiversonoticias.com.br/wp-content/uploads/2023/08/jornal-nacional.jpg"
+  },
+  {
+    "id":"2",
+    "title":"TITULO DO CARD 2",
+    "description": "ola",
+    "photoCover":"https://t4.ftcdn.net/jpg/01/10/91/67/360_F_110916739_rpdUPryXBJtCLCF6RDXpPMBNnLlc4Vqm.jpg"
+  }
+]
